c1cb1bab84755edbb639a3e118419669c7ff44b8
tixxff <awatif5166@gmail.com>
2021-09-06 20:43:58 +0700

    create component navbar and employee

--- customerApp/src/app/app.module.ts
+++ customerApp/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const appRoutes: Routes = [
+  
+  { path: '', component:EmployeesComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
